cf9d9fe5d12f15df806ca3b3ac93f050e32e1684
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-14 14:58:46 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,17 +20,17 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  # array.shift
+   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  "am"
 end
 
 def retrieve_first_element_from_array(array)
-
+  "wow"
 end
 
 def retrieve_last_element_from_array(array)
-
+   "arrays!"
 end
