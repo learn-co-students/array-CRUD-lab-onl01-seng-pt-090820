77b636e41a583bcbc96c5f459ab9841a9109e0b7
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-14 14:48:27 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,19 +8,19 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << "arrays!"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift "wow"
+  array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
-    create_an_array.pop
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-
+  # array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
