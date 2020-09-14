3dda60ddc74f6353bab9fcd3e696cbfa6bfdb398
global-listener-7039 <jodiesternal@yahoo.com>
2020-09-14 14:34:47 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,20 +3,20 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array = Array.new
+  ["dog", "cat", "fish", "turtle"]
 
 end
 
 def add_element_to_end_of_array(array, element)
-
+  create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-
+  create_an_array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
-
+    create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
