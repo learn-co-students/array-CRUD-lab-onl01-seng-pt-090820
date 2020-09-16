973f08d01dbbefaf845b4fe9cf99ac98223c56dc
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-16 05:46:43 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,8 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  famous_cats =  ["Cheshire Cat", "Puss in Boots", "Garfield"]
-  famous_cats[0]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
