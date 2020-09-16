7290586a01877e95fa50fe900ca6e619a9ea22a9
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-16 01:59:53 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,62 @@
 def create_an_empty_array
-  
+  array= []
 end
+
+
 
 def create_an_array
-  
+  array = ["Karen", "Helen", "Claire", "David"]
 end
+
+
 
 def add_element_to_end_of_array(array, element)
-  
+   array = ["I", "am", "really" "learning"]
+  array.push ("arrays!")
 end
+
+
 
 def add_element_to_start_of_array(array, element)
-  
+   array.unshift("wow")
 end
 
-def remove_element_from_end_of_array(array)
-  
-end
+
 
 def remove_element_from_start_of_array(array)
-  
+     array.shift 
+ end
+
+
+    
+def remove_element_from_end_of_array(array)
+  array.pop 
 end
+
+
+
+
+def array #retrieve_last_element_from_array
+    array #last 
+end
+    
+
+
 
 def retrieve_element_from_index(array, index_number)
-  
+  name = array[2]
 end
+
+
+
 
 def retrieve_first_element_from_array(array)
-  
+   array.first
 end
 
+
+
 def retrieve_last_element_from_array(array)
-  
+    array.last 
 end
+
